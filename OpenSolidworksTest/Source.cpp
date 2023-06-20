1d1e51c3549d8dc60ac8095d4e71eb8f5f76175f
SeanDKelly <stsekell@hochschulekempten.onmicrosoft.com>
2023-06-20 18:35:47 +0200

    Teil läst sich öffnen

--- OpenSolidworksTest/Source.cpp
+++ OpenSolidworksTest/Source.cpp
@@ -11,37 +11,50 @@ int main()
     {
         // COM Pointer of Soldiworks object
         CComPtr<ISldWorks> swApp;
+        CComPtr<IModelDoc2> model;
 
         // SolidWorks-Anwendung erstellen
         //SldWorks::ISldWorksPtr swApp;
 
         HRESULT hr = swApp.CoCreateInstance(__uuidof(SldWorks));
-        if (FAILED(hr))
+        if (swApp.CoCreateInstance(__uuidof(SldWorks), NULL, CLSCTX_LOCAL_SERVER) != S_OK)
         {
             std::cout << "Fehler beim Erstellen der SolidWorks-Anwendung!" << std::endl;
-            CoUninitialize();
-            return -1;
+            CoUninitialize(); // COM schnittstelle schliesen
+            return(0);
+        }
+
+        else {
+            std::cout << "SW erfolgreich geoeffnet" << std::endl;
         }
    
         // SolidWorks-Datei öffnen
 
 
         // COM Pointer of Soldiworks Model Document
-        CComPtr<IModelDoc2> model;
-        VARIANT_BOOL status = VARIANT_FALSE;
+        
         BSTR dateiPfad = SysAllocString(L"C:\\Users\\theja\\Downloads\\Greifer.SLDPRT");
-        BSTR config = SysAllocString(L"");
+        BSTR config = SysAllocString(L"Default");
+        CComBSTR sDefaultConfiguration(L"Default");
+
+        int documentType = (int)swDocumentTypes_e::swDocPART;
+        long lErrors;
+        long lWarnings;
+        long lMateError;
+
 
         swApp->OpenDoc6(
-            dateiPfad, // Pfad zur SolidWorks-Datei
-            1,      // Dateityp: 1 für Teil, 2 für Baugruppe, usw.
-            1,      // Optionen: 1 für Aktualisieren des Ansichtsbereichs, 0 für Standardoptionen
-            config, // Keine spezifische Konfiguration
-            0,   // Keine Fehler-Rückgabe
-            0,   // Keine Warnungen-Rückgabe
-            &model
-        );
+            dateiPfad,
+            swDocPART,
+            swOpenDocOptions_Silent,
+            sDefaultConfiguration,
+            &lErrors,
+            &lWarnings,
+            &model);
         
+        swApp->put_Visible(VARIANT_TRUE);
+
+
 
         if (model != NULL) {
             std::wcout << "Erfolgreich geoeffnet" << std::endl;
@@ -49,12 +62,13 @@ int main()
 
         else {
             std::wcout << "Fehler beim oeffnen" << std::endl;
+            // Beenden der SolidWorks-Anwendung
+            swApp->ExitApp();
         }
 
     
 
-        // Beenden der SolidWorks-Anwendung
-        swApp->ExitApp();
+        
 
         // Bereinigung der COM-Bibliothek
 
