6440ff5dfa18798913d77948bed33e28933e090b
SeanDKelly <stsekell@hochschulekempten.onmicrosoft.com>
2023-06-20 20:23:35 +0200

    Erfolgreich in eine Klasse umgewandelt

--- OpenSolidworksTest/Source.cpp
+++ OpenSolidworksTest/Source.cpp
@@ -3,47 +3,63 @@
 #import "sldworks.tlb" raw_interfaces_only, raw_native_types, no_namespace, named_guids //the SOLIDWORKS type library
 #import "swconst.tlb"  raw_interfaces_only, raw_native_types, no_namespace, named_guids //the SOLIDWORKS constant type library
 
-int main()
-{
-    // Initialisierung der COM-Bibliothek
-    CoInitialize(NULL);
+class SolidWorks {
+public:
+    // COM Pointer of Soldiworks object
+    CComPtr<ISldWorks> SW;
 
-    {
-        // COM Pointer of Soldiworks object
-        CComPtr<ISldWorks> swApp;
-        CComPtr<IModelDoc2> model;
-
-        // SolidWorks-Anwendung erstellen
-        //SldWorks::ISldWorksPtr swApp;
-
-        HRESULT hr = swApp.CoCreateInstance(__uuidof(SldWorks));
-        if (swApp.CoCreateInstance(__uuidof(SldWorks), NULL, CLSCTX_LOCAL_SERVER) != S_OK)
+    // Konstruktor
+    SolidWorks() {
+        // Initialisierung der COM-Bibliothek
+        CoInitialize(NULL);
+        // create Solidworks instance
+        HRESULT hr = SW.CoCreateInstance(__uuidof(SldWorks));
+        if (SW.CoCreateInstance(__uuidof(SldWorks), NULL, CLSCTX_LOCAL_SERVER) != S_OK)
         {
-            std::cout << "Fehler beim Erstellen der SolidWorks-Anwendung!" << std::endl;
-            CoUninitialize(); // COM schnittstelle schliesen
-            return(0);
+            std::cout << "Fehler beim öffnen von Solidworks" << std::endl;
         }
 
         else {
-            std::cout << "SW erfolgreich geoeffnet" << std::endl;
+            std::cout << "Solidworks erfolgreich geoeffnet" << std::endl;
         }
-   
-        // SolidWorks-Datei öffnen
-
-
-        // COM Pointer of Soldiworks Model Document
         
-        BSTR dateiPfad = SysAllocString(L"C:\\Users\\theja\\Downloads\\Greifer.SLDPRT");
+    };
+
+    // Destruktor
+    ~SolidWorks() {
+        //SW->ExitApp();// Beenden der SolidWorks-Anwendung
+        CoUninitialize(); // COM schnittstelle schliesen
+    }
+
+    // Teil Öffnen
+    void OpenPart(BSTR dateiPfad) {
+
+        // Instanz von SW Bauteil Klasse welches unser zu öffnendes 
+        // Bauteil halten wird
+        CComPtr<IModelDoc2> model;
+
+
+        // config mit der wir die Datei öffnen wir nehmen hie die
+        // Default config
         BSTR config = SysAllocString(L"Default");
         CComBSTR sDefaultConfiguration(L"Default");
 
+
+        // Die Öffnen Funktion will wissen ob es sich um ein Bautel,
+        // Zeichnung oder Baugruppe handelt. Hier öffnen wir ein PART also
+        // ein Bauteil.
         int documentType = (int)swDocumentTypes_e::swDocPART;
+        
+        
+        // Variable um Fehler zu speichern
         long lErrors;
+
+        // Variable um Warnunge nzu speichern
         long lWarnings;
-        long lMateError;
 
 
-        swApp->OpenDoc6(
+        // Bauteil öffnen
+        SW->OpenDoc6(
             dateiPfad,
             swDocPART,
             swOpenDocOptions_Silent,
@@ -51,30 +67,30 @@ int main()
             &lErrors,
             &lWarnings,
             &model);
-        
-        swApp->put_Visible(VARIANT_TRUE);
-
 
 
         if (model != NULL) {
-            std::wcout << "Erfolgreich geoeffnet" << std::endl;
+            std::wcout << "Teil erfolgreich geoeffnet" << std::endl;
         }
 
         else {
-            std::wcout << "Fehler beim oeffnen" << std::endl;
-            // Beenden der SolidWorks-Anwendung
-            swApp->ExitApp();
+            std::wcout << "Fehler beim oeffnen des Teils" << std::endl;
         }
-
-    
-
-        
-
-        // Bereinigung der COM-Bibliothek
-
-
     }
-    CoUninitialize();
+};
 
+
+
+int main()
+{
+    SolidWorks SW;
+    
+    // Hier den Entsprechenden Dateipfad eingebn zu dem Teil das man öffnen will
+    CComBSTR dateiPfad = (L"C:\\Users\\theja\\Downloads\\Greifer.SLDPRT");
+    
+    
+    SW.OpenPart(dateiPfad);
     return 0;
 }
+
+// swApp->put_Visible(VARIANT_TRUE);
